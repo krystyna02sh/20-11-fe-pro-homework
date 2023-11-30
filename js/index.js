2ee90642b6e12845b1c4a7502beed9bccdd9298c
Krystyna Shunkina <christina.shunkina@gmail.com>
2023-11-30 23:37:05 +0200

    homework3second try

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-const input = prompt('Enter a number from 1 to 4');
+const input = +prompt('Enter a number from 1 to 4');
+
 switch (input) {
-    case "1":
+    case 1:
         alert('Today is a good day to start something');
         break;
-    case "2":
+    case 2:
         alert('See your friend today');
         break;
-    case "3":
+    case 3:
         alert('Buy yourself something tasty today');
         break;
-    case "4":
+    case 4:
         alert('Do your HW today');
         break;
     default:
