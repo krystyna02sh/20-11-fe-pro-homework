7103e546253081e18f7b52e631b7de0588be3846
Krystyna Shunkina <christina.shunkina@gmail.com>
2023-12-08 23:02:38 +0200

    homework4 done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+const numbers = [5, 8, -1, -6, 11, 0, -2]
+let sum = 0
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+}
+
+alert('Sum is ' + sum)
+
+
